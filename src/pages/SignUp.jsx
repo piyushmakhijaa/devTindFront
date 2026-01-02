dab0b2ec61dd9efae52f9c6e154fbbc8fdd4855e
piyushmakhijaa <piyushmakhija04@gmail.com>
2026-01-02 13:06:10 +0530

    errors corrected while build

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 function SignUp(){
 
